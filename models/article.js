a06d7df918cda9f96e7fdeea063700e07507d9ef
mrassett <magsrassett@gmail.com>
2018-06-23 08:26:02 -0500

    incomplete

---
+++ models/article.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var ArticleSchema = new Schema({
+  // `title` is required and of type String
+  title: {
+    type: String,
+    required: true
+  },
+link: {
+    type: String,
+    required: true
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+
+module.exports = Article;
